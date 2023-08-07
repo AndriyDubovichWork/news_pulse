174c87c8b37ad51bed98deef73016c9e1eb38c09
Andriy Dubovich <andriydubovichwork@gmail.com>
2023-08-07 10:28:12 +0300

    links

--- src/app/Layout/Header.tsx
+++ src/app/Layout/Header.tsx
@@ -10,7 +10,10 @@ export default function Header() {
         <Link href='/news' className={style.logo}>
           News Pulse
         </Link>
-        <Link href='/categories'>categories</Link>
+        <Link href='/categories'>Categories</Link>
+        <Link href='/pages'>Pages</Link>
+        <Link href='/contact-us'>Contact us</Link>
+        <Link href='/about-us'>About us</Link>
       </div>
       <div className={style.half}>
         <p>News Pulse</p>
