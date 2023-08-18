5984101e27b9644fac6cdb1bdb1ce2435a564fde
Andriy Dubovich <andriydubovichwork@gmail.com>
2023-08-18 20:54:48 +0300

    created footer

--- src/app/Layout/Footer.tsx
+++ src/app/Layout/Footer.tsx
@@ -4,7 +4,17 @@ import style from './footer.module.scss';
 export default function Footer() {
   return (
     <footer className={style.footer}>
-      <div>hello</div>
+      <div>
+        <h3>Mega News</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas
+          purus viverra accumsan in nisl nisi. Arcu cursus vitae congue mauris
+          rhoncus aenean vel elit scelerisque. In egestas erat imperdiet sed
+          euismod nisi porta lorem mollis. Morbi tristique senectus et netus.
+          Mattis pellentesque id nibh tortor id aliquet lectus proin
+        </p>
+      </div>
     </footer>
   );
 }
