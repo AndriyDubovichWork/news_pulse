8e60c4b1b0c68be2d29a157f8dd2119f0a82af0a
Andriy Dubovich <andriydubovichwork@gmail.com>
2023-08-23 20:37:00 +0300

    football widget

--- src/app/components/Widgets/FootBallWidget/FootBallWidget.tsx
+++ src/app/components/Widgets/FootBallWidget/FootBallWidget.tsx
@@ -1,7 +1,13 @@
 import Image from 'next/image';
 import React from 'react';
 import style from './FootBallWidget.module.scss';
+import calendar from './imgs/calendar.png';
+import club from './imgs/club-table.png';
+import competition from './imgs/competition.png';
+
 export default function FootBallWidget() {
+  const images = [calendar, club, competition];
+
   return (
     <div className={style.footBall}>
       <Image
@@ -11,7 +17,19 @@ export default function FootBallWidget() {
         src='https://c4.wallpaperflare.com/wallpaper/398/874/541/champions-league-stadium-wallpaper-preview.jpg'
         alt='socer field image'
       />
-      
+      <div className={style.content}>
+        {images.map((imgSrc, id) => {
+          const isMiddle = id === 1;
+
+          return (
+            <Image
+              src={imgSrc}
+              alt='football data images'
+              className={`${isMiddle && style.middle}`}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
