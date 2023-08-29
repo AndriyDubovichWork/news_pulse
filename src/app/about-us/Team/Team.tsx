272e7b7d3358380406b97c7efda5e111bbf3ed3c
Andriy Dubovich <andriydubovichwork@gmail.com>
2023-08-29 22:21:37 +0300

    team members about info

---
+++ src/app/about-us/Team/Team.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import style from './Team.module.scss';
+import Title from '@/app/components/Title/Title';
+import Image from 'next/image';
+
+type MemberT = {
+  position: string;
+  name: string;
+};
+
+const members: MemberT[] = [
+  { position: 'Designer', name: 'Behzad Pashaei' },
+  { position: 'Programmer', name: 'Cassie Evans' },
+  { position: 'Marketing', name: 'Louis Hoebregts' },
+  { position: 'Administrative', name: 'Patricia' },
+  { position: 'CEO', name: 'James Hoebregts' },
+  { position: 'Financial', name: 'Jon Kantner' },
+];
+
+function Member({ member }: { member: MemberT }) {
+  return (
+    <div className={style.member}>
+      <Image
+        className={style.img}
+        width={124}
+        height={124}
+        alt={member.name + 'image'}
+        src={`https://random.imagecdn.app/124/124/?avoidCachingSoItwillBeDifferentImages=${member.name}`}
+      />
+      <p className={style.name}>{member.name}</p>
+      <p className={style.position}>{member.position}</p>
+    </div>
+  );
+}
+
+function Team() {
+  return (
+    <>
+      <Title title='News Pulse Team' className={style.title} />
+      <div className={style.team}>
+        {members.map((member) => {
+          return <Member member={member} />;
+        })}
+      </div>
+    </>
+  );
+}
+
+export default Team;
--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -3,12 +3,14 @@ import style from './aboutUs.module.scss';
 import ArrowedLink from '../components/ArrowedLink/ArrowedLink';
 import Explanation from './Explanation/Explanation';
 import Info from './Info/Info';
+import Team from './Team/Team';
 function aboutUs() {
   return (
     <div className={style.aboutUs}>
       <ArrowedLink current='About Us' />
       <Explanation />
       <Info />
+      <Team />
     </div>
   );
 }
