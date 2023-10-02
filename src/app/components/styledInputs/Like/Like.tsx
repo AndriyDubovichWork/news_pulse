2f45c449f2443de23176c9ea5a62d1ffe74a84b7
Andriy Dubovich <86286088+AndriyDubovichWork@users.noreply.github.com>
2023-10-02 09:00:42 +0300

    likes counter

--- src/app/components/styledInputs/Like/Like.tsx
+++ src/app/components/styledInputs/Like/Like.tsx
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
 import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai';
 import style from './like.module.scss';
+import randomNumStrFormat from '@/app/lib/generateRandomNumberInStringFormat';
 function Like() {
   const [active, setActive] = useState(false);
   const [isLiked, setIsLiked] = useState(false);
+
+  const [likes] = useState(randomNumStrFormat(1_000, 30_000));
   return (
-    <div>
+    <div className={style.likesContainer}>
+      {likes && <h3>{likes}</h3>}
       {active ? (
         <AiFillHeart
           className={style.active}
