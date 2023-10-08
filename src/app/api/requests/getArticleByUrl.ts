0474cc21035ade932a56cca047b2ef26aa053423
Andriy Dubovich <86286088+AndriyDubovichWork@users.noreply.github.com>
2023-10-08 17:27:44 +0300

    removed cors correnct url request

--- src/app/api/requests/getArticleByUrl.ts
+++ src/app/api/requests/getArticleByUrl.ts
@@ -3,7 +3,7 @@ import React from 'react';
 
 async function getArticleByUrl(url: string) {
   return await axios
-    .get(`http://localhost:3000/api/article?url=${url}`)
+    .get(`https://news-pulse-livid.vercel.app/api/article?url=${url}`)
     .then((res) => {
       return res.data;
     });
